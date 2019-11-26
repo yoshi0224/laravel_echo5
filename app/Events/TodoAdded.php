e1b8b3c9cd1bb0dc5f8864fcc2f773f96494e1fa
yoshi0224 <matumatu0224@gmail.com>
2019-11-26 15:38:58 +0900

    完璧

---
+++ app/Events/TodoAdded.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class TodoAdded implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $todo;
+
+    public function __construct($todo)
+    {
+        $this->todo = $todo;
+    }
+
+    public function broadcastOn()
+    {
+        return new Channel('todo-added-channel',$this->todo);
+    }
+}
